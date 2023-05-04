34d325eaa0a95b8d4f47941a3b4d572e6e51581c
Eamith <yoesam36962@stud.noroff.no>
2023-05-04 14:13:59 +0200

    update

--- JS/detail.js
+++ JS/detail.js
@@ -6,10 +6,6 @@ const params = new URLSearchParams (querystring);
 
 const id= params.get ("id")
 
-console.log(id);
-
-const url = "https://api.noroff.dev/api/v1/rainy-days/" + id;
-
 async function fetchGame() {
 
     try {
@@ -28,11 +24,24 @@ async function fetchGame() {
     
 }
 
-fetchGame();
+fetchProducts("https://api.noroff.dev/api/v1/rainy-days"/ + id);
 
 function createHtml(details) {
-    detailContainer.innerHTML = `<h1>${details.name}</h1>
-                                <div class="details-image" 
-                                    style="background-image: url('${details.background_image}')"></div>
-                                <div class="details-description">${details.description}</div>
-                                <time class="details-date">Released: ${details.released}</time>`;
+    detailContainer.innerHTML = `<div class="product">
+            
+
+    <a href="Jacketspecificpage.html?id=${products.id}" class="img"><img src="${response[i].image}" alt="" class="product-img" /></a>
+    
+    <div class="product-content">
+    <h2 class="product-title">${
+      title.lenght > 18 ? title.substring(0, 18).concat("  ...") : title
+    }</h2>
+    <p class="product-sizes">Size - ${response[i].sizes}</p>
+    <div class="product-price-container">
+        <h3 class="product-price">$ ${response[i].price}</h3>
+        <a href="Checkoutpage.html" data-productId="${
+          response[i].id
+        }" class="add-to-cart"><ion-icon name="cart-outline"></ion-icon></a>
+    </div>
+    </div>
+</div>`;
